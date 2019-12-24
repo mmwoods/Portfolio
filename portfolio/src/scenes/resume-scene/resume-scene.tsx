e287a4534d8e7b506258d76adac3e0364d6ab0c3
Mitchell Woods <mw@mitchellmwoods.com>
2019-12-24 13:07:22 +1000

    feat: initial resume scene

---
+++ portfolio/src/scenes/resume-scene/resume-scene.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import { StackNavigation } from "../../components/";
+
+export const ResumeScene = () => (
+  <StackNavigation>
+    <p>Resume</p>
+  </StackNavigation>
+);
