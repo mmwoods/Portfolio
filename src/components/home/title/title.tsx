64e011a6b9d032f66e55004df7dfe40f248bc453
Mitchell Woods <mw@mitchellmwoods.com>
2019-12-28 12:31:57 +1000

    fix: mobile overlap issue

--- src/components/home/title/title.tsx
+++ src/components/home/title/title.tsx
@@ -5,8 +5,8 @@ import css from "./title.module.css";
 export const Title = () => (
   <div className={css.wrapper}>
     <div className={css.container}>
-      <h1 className={css.title}>Projects I have been working on</h1>
-      <p className={css.subtitle}>Currently focusing on Serverless/DynamoDB</p>
+      <h1 className={css.title}>Projects Ongoing</h1>
+      <p className={css.subtitle}>Currently focusing on Serverless</p>
     </div>
   </div>
 );
