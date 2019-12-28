fc79488e9cc36a0b17460fd13295c8322794e391
Mitchell Woods <mw@mitchellmwoods.com>
2019-12-28 11:34:41 +1000

    feat: updating experience styles

--- src/components/home/experience/experience.tsx
+++ src/components/home/experience/experience.tsx
@@ -1,45 +1,18 @@
 import React, { CSSProperties } from "react";
 
-export const Experience = () => (
-  <div style={styles.wrapper}>
-    <h1 style={styles.title}>Experience with the web</h1>
-    <h2 style={styles.subtitle}>Commercial &amp; Freelance</h2>
+import css from "./experience.module.css";
 
-    <p style={styles.description}>
-      In 6+ years of web development I have scoped, designed, coded, and
-      maintained eCommerce and static websites. As well as managing design
-      projects, website go-lives and more.
-    </p>
+export const Experience = () => (
+  <div className={css.wrapper}>
+    <div className={css.container}>
+      <h1 className={css.title}>Experience with the web</h1>
+      <h2 className={css.subtitle}>Commercial &amp; Freelance</h2>
+
+      <p className={css.description}>
+        In 6+ years of web development I have scoped, designed, coded, and
+        maintained eCommerce and static websites. As well as managing design
+        projects, website go-lives and more.
+      </p>
+    </div>
   </div>
 );
-
-interface ExperienceStyles {
-  wrapper: CSSProperties;
-  title: CSSProperties;
-  subtitle: CSSProperties;
-  description: CSSProperties;
-}
-
-const styles: ExperienceStyles = {
-  wrapper: {
-    textAlign: "left",
-    maxWidth: 600,
-    margin: "75px auto auto"
-  },
-  title: {
-    fontSize: 32,
-    color: "#333",
-    fontWeight: 300
-  },
-  subtitle: {
-    fontSize: 16,
-    color: "#95bbdb",
-    fontWeight: 400,
-    textTransform: "uppercase"
-  },
-  description: {
-    color: "rgba(14, 30, 37, 0.54)",
-    lineHeight: "22px",
-    marginBottom: "18px"
-  }
-};
