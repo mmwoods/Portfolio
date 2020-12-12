a8b0ca9bf96c568fb1b40fc19fc5f186a3f74ca9
Mitchell Woods <mw@mitchellmwoods.com>
2020-12-12 22:08:00 +1000

    fix: made experience years dynamic

--- src/components/home/experience/experience.tsx
+++ src/components/home/experience/experience.tsx
@@ -2,17 +2,21 @@ import React from "react";
 
 import css from "./experience.module.css";
 
-export const Experience = () => (
+export const Experience = () => {
+  const year = new Date().getFullYear()
+  const experience = year - 2013
+
+  return (
   <div className={css.wrapper}>
     <div className={css.container}>
       <h1 className={css.title}>Experience with the web</h1>
       <h2 className={css.subtitle}>Commercial &amp; Freelance</h2>
 
       <p className={css.description}>
-        In 6+ years of web development I have scoped, designed, coded, and
+        In {experience}+ years of web development I have scoped, designed, coded, and
         maintained eCommerce and static websites. As well as managing design
         projects, website go-lives and more.
       </p>
     </div>
   </div>
-);
+)}
